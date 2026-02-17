a4d66bc7aa00365f00ae9ebd701be2ab939e2538
ahmednayem898-beep <ahmednayem898@gmail.com>
2026-02-17 04:26:49 -0800

    update task

--- firebase/init.py
+++ firebase/init.py
@@ -3,10 +3,10 @@ from firebase_admin import credentials, firestore
 import os
 cred = credentials.Certificate({
     "type": "service_account",
-    "project_id": os.environ["FIREBASE_PROJECT_ID"],
-    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
-    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
-    "token_uri": os.environ["FIREBASE_CLIENT_URL"] 
+    "project_id": os.getenv["FIREBASE_PROJECT_ID"],
+    "private_key": os.getenv["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
+    "client_email": os.getenv["FIREBASE_CLIENT_EMAIL"],
+    "token_uri": os.getenv["FIREBASE_CLIENT_URL"] 
 })
 firebase_admin.initialize_app(cred)
 db = firestore.client()
