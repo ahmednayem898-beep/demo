30ad31ffa95a831f8d8211969abf52eff63571f2
ahmednayem898-beep <ahmednayem898@gmail.com>
2026-02-17 05:12:38 -0800

    update task

--- main.py
+++ main.py
@@ -54,16 +54,16 @@ async def interactions(request: Request):
                 }
             })
         elif command_name == "doc" :
-            question = data["data"]["options"][0]["value"]
-            ai_response = ask_doc(question)
-            if ai_response and ai_response['success']:
-                return JSONResponse({
-                    "type": 4,  
-                    "data": {
-                        "content": ai_response['msg']
-                    }
-                })
-            else:
+            # question = data["data"]["options"][0]["value"]
+            # ai_response = ask_doc(question)
+            # if ai_response and ai_response['success']:
+            #     return JSONResponse({
+            #         "type": 4,  
+            #         "data": {
+            #             "content": ai_response['msg']
+            #         }
+            #     })
+            # else:
                 return JSONResponse({
                     "type": 4,  
                     "data": {
